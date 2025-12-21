897b09951f9762edf6a80510a70297868c3f1b6f
ineedjobtostartwork <ineedjobtostartwork@gmail.com>
2025-12-22 00:11:44 +0100

    🎉 Feat(typescript-utils,enviroment): init files!

--- packages/test-package/src/bin/app.ts
+++
@@ -1,5 +0,0 @@
-// eslint-disable-next-line @EslintImports/no-unassigned-import
-import "@/cli";
-import { program } from "commander";
-
-program.parse(process.argv);
--- packages/test-package/src/cli/version.ts
+++
@@ -1,8 +0,0 @@
-import { version } from "@/../package.json";
-import { program } from "commander";
-
-//----------------------
-// CLI APP
-//----------------------
-
-program.version(version);
